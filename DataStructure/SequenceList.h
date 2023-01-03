02f6461f9b1f799dc6c8b27cb61e25627a726270
三 张 <230888962@qq.com>
2023-01-03 14:56:11 +0800

    线性表部分全部更新完成

--- DataStructure/SequenceList.h
+++ DataStructure/SequenceList.h
@@ -29,6 +29,7 @@ const int maxSize = 100;  //推荐写法
 		5.查：查找线性表中值为x的元素
 		6.查：查找线性表中第i个位置的元素
 		7.改：将线性表中值为x的第一个元素的值改为y
+		8.打印：输出线性表所有元素
 
 */
 
@@ -66,9 +67,78 @@ bool insertXLinearList(LinearList* list, int i, int x) {
 //3.删：删除线性表中值为x的元素
 bool delXLinearList(LinearList* list, int x) {
 	//1.判误
-	if (!list || list->num == 0) return true;
+	if (!list || list->num == 0) return true;  //删除完成
 	//2.找值为x的元素的下标，找到后将后边元素向前移动一位
 	for (int i = 0; i < list->num; i++) {
-		
+		if (list->arr[i] != x) continue;  //i位置的值不是要找的
+		else {
+			for (; i < list->num - 1; i++) list->arr[i] = list->arr[i + 1];
+			list->num--;
+			return true;
+		}
 	}
+	return true;
+}
+
+//4.删：删除线性表中第i个位置的元素
+bool delILinearList(LinearList* list, int i) {
+	//1.判误
+	if (i <= 0 || i > list->num || !list || list->num == 0)return false;
+	//2.找第i个位置的元素，并将其后元素前移一位
+	for (; i < list->num-1; i++) {
+		list->arr[i] = list->arr[i + 1];
+	}
+	list->num--;
+	return true;
+
+}
+
+//5.查：查找线性表中值为x的元素,返回下标，没找到返回-1
+int searchXLinearList(LinearList* list, int x) {
+	//1.判误
+	if (!list || list->num == 0) return -1;
+	//2.找
+	for (int i = 0; i < list->num; i++) {
+		if (list->arr[i] != x)continue;//i位置的值不是我要找的x
+		else {//找到x的值，并进行操作
+			return i;
+		}
+	}
+	//3.for完了没找到
+	return -1;
+}
+
+//6.查：查找线性表中第i个位置的元素
+bool searchILinearList(LinearList* list, int i,int* e) {
+	//1.判误
+	if (!list || list->num == 0 || i <= 0 || i > list->num)return false;
+	//2.开始查找
+	*e = list->arr[i - 1];
+	return true;
+}
+
+//7.改：将线性表中值为x的第一个元素的值改为y
+bool changeXtoYLinearList(LinearList* list, int x, int y) {
+	//1.判误
+	if (!list || list->num == 0)return true;//修改成功
+	//2.找x，找到后改成y - 只修改第一个找到的元素
+	for (int i = 0; i < list->num;i++) {
+		if (list->arr[i] == x) {
+			list->arr[i] = y;
+			return true;
+		}
+	}
+	//3.没找到x-默认修改成功
+	return true;
+}
+
+//8.打印：输出线性表所有元素
+void showLinearList(LinearList* list) {
+	//1.判误
+	if (!list || list->num == 0)return;
+	for (int i = 0; i < list->num; i++) {
+		printf("%d ", list->arr[i]);
+	}
+	puts("");
+
 }
--- DataStructure/main.c
+++ DataStructure/main.c
@@ -4,13 +4,20 @@ int main(void) {
 	//线性表调用
 	LinearList* myList = initLinearList();//初始化线性表
 	printf("线性表是否创建成功=%d\n",!!myList);
-	printf("插入是否成功 = %d\n",insertXLinearList(myList, 1, 3));
-	myList->num = 20;
-	insertXLinearList(myList, 10, 2);
-	for (int i = 0; i < maxSize; i++)
-	{
-		printf("%d", myList->arr[i]);
+	for (int i = 0; i < 10; i++) {
+		insertXLinearList(myList, i + 1, rand() % 10);//1 7 4 0 9 4 8 8 2 4
 	}
+	showLinearList(myList);//展示线性表
+	delXLinearList(myList, 4);
+	showLinearList(myList);
+	delILinearList(myList, 6);
+	showLinearList(myList);
+	printf("下标=%d", searchXLinearList(myList, 8));
+	int e = 0;
+	if (searchILinearList(myList, 3, &e)) printf("e = %d\n", e);
+	else printf("search I false\n");
+
+
 
 	return 0;
 }
